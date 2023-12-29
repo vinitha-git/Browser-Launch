3e225f639a5ec4cfd52d806859381f5d1a08c855
Vinitha Edwin <vinithaedwin@gmail.com>
2023-12-29 08:55:17 +0530

    Invalid Credentials

--- src/main/java/org/selenium/constants/Messages.java
+++ src/main/java/org/selenium/constants/Messages.java
@@ -2,6 +2,6 @@ package org.selenium.constants;
 public class Messages {
     public static final String TITLE_MISMATCH= "Invalid Title";
     public static final String USER_REGISTRATION_FAILED="User Registration failed";
-    public static final String LOGIN_FAILED="Login failed";
+    public static final String LOGIN_FAILED_MESSAGE="Login failed warning message not found";
     public static final String VALUE_SELECTED="Value selected";
 }
